3e6e2a3e5a8eb10ce51efe2f71e569b8613c8ae4
alcetta <ralcetta12@gmail.com>
2025-04-15 10:03:33 +0200

    Update userController.js

--- controllers/userController.js
+++ controllers/userController.js
@@ -65,6 +65,35 @@ const updateUser = asyncHandler(async (req, res) => {
       };
     }
 
+    exports.loginUser = async (req, res, next) => {
+  try {
+    const { email, password } = req.body;
+
+    // Validate email & password
+    if (!email || !password) {
+      return next(new ErrorResponse('Please provide an email and password', 400));
+    }
+
+    // Check for user
+    const user = await User.findOne({ email }).select('+password');
+
+    if (!user) {
+      return next(new ErrorResponse('Invalid credentials', 401));
+    }
+
+    // Check if password matches
+    const isMatch = await user.matchPassword(password);
+
+    if (!isMatch) {
+      return next(new ErrorResponse('Invalid credentials', 401));
+    }
+
+    sendTokenResponse(user, 200, res);
+  } catch (err) {
+    next(err);
+  }
+};
+
     const updatedUser = await user.save();
 
     res.json({
